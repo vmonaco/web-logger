09abc95b030868d2087ae3431ede7633f65b5aa4
Vinnie Monaco <contact@vmonaco.com>
2020-03-04 15:57:14 -0800

    updated weblogger to use sendbeacon

--- main.py
+++ main.py
@@ -5,55 +5,32 @@ import json
 from datetime import datetime
 from flask import Flask, render_template, request, url_for, redirect, jsonify, make_response
 
-THIS_DIR = os.path.dirname(os.path.realpath(__file__))
-ROOT_DIR = os.path.join(THIS_DIR, '..')
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
+DATA_DIR = '~/weblogger-data'
 
-SESSIONS_FNAME = 'sessions'
-METADATA_FNAME = 'metadata'
-METADATA_HEADER = 'id,session,task,date,useragent'
-
-# How often clients should flush logged events
-FLUSH_DELAY = 2000
-
-# Headers for data CSV files
 EVENT_HEADERS = {
-    'messages':'time,message,response',
-    'keystroke':'timepress,timerelease,keycode,keyname,target,selectionstartpress,selectionendpress,selectionstartrelease,selectionendrelease',
-    'mousemotion':'time,x,y,xpage,ypage,xtarget,ytarget,targetwidth,targetheight,dragged,target',
-    'mouseclick':'timepress,timerelease,button,xpress,ypress,xoffsetpress,yoffsetpress,targetwidthpress,targetheightpress,targetpress,selectionstartpress,selectionendpress,xrelease,yrelease,xoffsetrelease,yoffsetrelease,targetwidthrelease,targetheightrelease,targetrelease,selectionstartrelease,selectionendrelease',
-    'mousescroll':'time,xdelta,ydelta,deltamode,deltafactor,x,y,xpage,ypage,xtarget,ytarget,targetwidth,targetheight,target'
+    'events':'event,time,date_time,performance_time,x,y,z',
+    'metadata':'field,value',
 }
 
+for event_type in EVENT_HEADERS.keys():
+    os.makedirs(os.path.join(DATA_DIR, event_type), exist_ok=True)
+
+# How often to flush logged events
+FLUSH_DELAY = 2000
+
 app = Flask(__name__)
-
-
-def lookup_session(session):
-    fname = os.path.join(DATA_DIR, SESSIONS_FNAME + '.csv')
-    templates = {}
-    with open(fname, mode='r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            templates[row[1]] = {'login':row[2],'chat':row[3]}
-
-    if session in templates.keys():
-        return templates[session]
-    else:
-        return None
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 def append_events(id, event_type, events):
-    os.makedirs(os.path.join(DATA_DIR, event_type), exist_ok=True)
     fname = os.path.join(DATA_DIR, event_type, id + '.csv')
 
     if os.path.exists(fname):
-        # Append to an existing capture file
         with open(fname, 'a') as f:
             f.write('\n' + events)
     else:
         print('Created file:', fname)
 
-        # Create a new capture file
         with open(fname, 'w') as f:
             f.write(EVENT_HEADERS[event_type] + '\n')
             f.write(events)
@@ -62,67 +39,33 @@ def append_events(id, event_type, events):
     return num_events
 
 
-def save_meta(id, task, session, timenow, useragent):
-    fname = os.path.join(DATA_DIR, METADATA_FNAME + '.csv')
-
-    if os.path.exists(fname):
-        with open(fname, 'r') as f:
-            for line in f.readlines():
-                if id in line.strip().split(','):
-                    return
-
-        with open(fname, 'a') as f:
-            f.write('\n%s,%s,%s,%s,"%s"' % (id, session, task, timenow, useragent))
-    else:
-        print('Created file:', fname)
-
-        # Create a new capture file
-        with open(fname, 'w') as f:
-            f.write(METADATA_HEADER)
-            f.write('\n%s,%s,%s,%s,"%s"' % (id, session, task, timenow, useragent))
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
-@app.route("/enroll", methods=['POST'])
-def enroll():
-    # Get the user ID
+@app.route("/events", methods=['POST'])
+def events():
     id = request.args.get('id')
-
-    num_enrolled = 0
-    for event_type in EVENT_HEADERS.keys():
-        if event_type in request.form.keys():
-            n = append_events(id, event_type, request.form[event_type])
-            num_enrolled += n
-
-    return 'Enrolled %d event(s)' % n
-
-
-@app.route("/metadata", methods=['POST'])
-def metadata():
-    # Get the user ID
-    id = request.args.get('id')
-
-    num_enrolled = 0
-    for event_type in request.form.keys():
-        n = append_metadata(id, metedata_type, request.form[meteadata_type])
-        num_enrolled += n
-
-    return 'Enrolled %d metadata' % n
-
-
-@app.route("/tmp",  methods=['GET','POST'])
-def index():
-    timenow = datetime.utcnow()
-    fname = timenow.strftime('%Y%m%d%H%M%S.%f')
-    enroll_url = url_for('enroll', id=fname)
-    resp = make_response(render_template('index.html', enroll_url=enroll_url, flush_delay=FLUSH_DELAY))
-    return resp
+    type = request.args.get('type')
+    n = 0
+    if type in EVENT_HEADERS.keys():
+        n += append_events(id, type, request.get_data(as_text=True))
+    return 'Saved %d event(s)' % n
 
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def index():
+    id = datetime.utcnow().strftime('%Y%m%d%H%M%S.%f')
+    events_url = url_for('events', id=id, _external=True)
+    print(events_url)
+    resp = make_response(render_template('index.html', events_url=events_url, flush_delay=FLUSH_DELAY))
+    return resp
 
 
 if __name__ == "__main__":
-    os.makedirs(DATA_DIR, exist_ok=True)
     app.run(host='0.0.0.0')
---
+++ static/weblogger.js
@@ -0,0 +1,146 @@
+function hook(element, evnt, funct){
+  if (element.attachEvent)
+   return element.attachEvent('on'+evnt, funct);
+  else
+   return element.addEventListener(evnt, funct, false);
+}
+
+function unhook(element, evnt, funct){
+  if (element.detachEvent)
+   return element.detachEvent('on'+evnt, funct);
+  else
+   return element.removeEventListener(evnt, funct, false);
+}
+
+function makeCSV(values) {
+    var finalVal = '';
+
+    for (var j = 0; j < values.length; j++) {
+        var innerValue =  values[j]===null?'':''+values[j];
+        var result = innerValue.replace(/"/g, '""');
+        if (result.search(/("|,|\n)/g) >= 0)
+            result = '"' + result + '"';
+        if (j > 0)
+            finalVal += ',';
+        finalVal += result;
+    }
+    return finalVal;
+}
+
+function sendData(url, data, onsuccess) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            console.log(xhr.responseText);
+            onsuccess();
+        } else {
+            console.error(xhr.statusText);
+        }
+      }
+    };
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(JSON.stringify(data));
+}
+
+function Weblogger(eventsURL, flushDelay) {
+    console.log('Weblogger init');
+
+    var that = this; // Used in several closures below
+
+    this.eventsURL = new URL(eventsURL);
+    this.eventsURL.searchParams.set('type', 'events');
+
+    this.metadataURL = new URL(eventsURL);
+    this.metadataURL.searchParams.set('type', 'metadata');
+
+    this.flushDelay = flushDelay;
+
+    this.buffer = [];
+
+    this.addEvent = function(e) {
+        that.buffer.push(e);
+    };
+
+    this.keypress = function(e) {
+        that.addEvent(['keypress', e.timeStamp, Date.now(), performance.now(), e.location, e.repeat, e.keyCode]);
+    };
+
+    this.keydown = function(e) {
+        that.addEvent(['keydown', e.timeStamp, Date.now(), performance.now(), e.location, e.repeat, e.keyCode]);
+    };
+
+    this.keyup = function(e) {
+        that.addEvent(['keyup', e.timeStamp, Date.now(), performance.now(), e.location, e.repeat, e.keyCode]);
+    };
+
+    this.mousedown = function(e) {
+        that.addEvent(['mousedown', e.timeStamp, Date.now(), performance.now(), e.screenX, e.screenY, e.button]);
+    };
+
+    this.mouseup = function(e) {
+        that.addEvent(['mouseup', e.timeStamp, Date.now(), performance.now(), e.screenX, e.screenY, e.button]);
+    };
+
+    this.mousemove = function(e) {
+        that.addEvent(['mousemove', e.timeStamp, Date.now(), performance.now(), e.screenX, e.screenY, e.buttons]);
+    };
+
+    this.wheel = function(e) {
+        that.addEvent(['mousewheel', e.timeStamp, Date.now(), performance.now(), e.deltaX, e.deltaY, e.deltaMode]);
+    };
+
+    this.events = {
+        keypress: that.keypress,
+        keydown: that.keydown,
+        keyup : that.keyup,
+        mousedown : that.mousedown,
+        mouseup : that.mouseup,
+        mousemove : that.mousemove,
+        wheel : that.wheel,
+    };
+
+    this.emptyBuffer = function () {
+        that.buffer = [];
+    };
+    
+    this.gatherEvents = function(e) {
+        return that.buffer.join('\n');
+    };
+
+    this.gatherMetadata = function () {
+        return [
+          ['user_agent',navigator.userAgent],
+          ['app_version',navigator.appVersion],
+        ].map(makeCSV).join('\n');
+    };
+
+    this.startLogging = function() {
+        console.log('Start logging');
+
+        for (e in this.events) {
+            hook(
+                window,
+                e,
+                that.events[e]
+            );
+        };
+
+        this.flushTimer = setInterval(function () {
+            if (that.buffer.length === 0) {
+                return;
+            }
+            navigator.sendBeacon(that.eventsURL, that.gatherEvents());
+        }, this.flushDelay);
+
+        hook(window, 'unload', function () {
+            navigator.sendBeacon(that.eventsURL, that.gatherEvents());
+        });
+
+        navigator.sendBeacon(that.metadataURL, that.gatherMetadata());
+    };
+};
